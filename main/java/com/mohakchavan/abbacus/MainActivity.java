e1a2698714ac635d9920bece5c75cad442ae40ed
Mohak Chavan <mohak44chavan@gmail.com>
2020-07-15 04:07:06 +0530

    Initial Commit.

---
+++ main/java/com/mohakchavan/abbacus/MainActivity.java
@@ -0,0 +1,24 @@
+package com.mohakchavan.abbacus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    DataBaseHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        helper = new DataBaseHelper(MainActivity.this);
+//        helper.copyDataBase();
+//        List<QuestionModel> questionModels = helper.getAllData();
+//        Toast.makeText(MainActivity.this, questionModels.get(0).getQuestionText(), Toast.LENGTH_SHORT).show();
+    }
+}
